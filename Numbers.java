d761ba06b6cf3cc9094e2c46b27abd8d0c1d93c7
achu310197 <achuu0197@gmail.com>
2019-10-30 10:10:51 +0530

    Done

---
+++ Numbers.java
@@ -0,0 +1,35 @@
+package com.aanchal.entities;
+
+
+public class Numbers {
+	
+	private int firstNumber;
+	private int secondNumber;
+	
+	public Numbers() {
+		
+	}
+
+	public Numbers(int firstNumber, int secondNumber) {
+		super();
+		this.firstNumber = firstNumber;
+		this.secondNumber = secondNumber;
+	}
+
+	public int getFirstNumber() {
+		return firstNumber;
+	}
+
+	public void setFirstNumber(int firstNumber) {
+		this.firstNumber = firstNumber;
+	}
+
+	public int getSecondNumber() {
+		return secondNumber;
+	}
+
+	public void setSecondNumber(int secondNumber) {
+		this.secondNumber = secondNumber;
+	}
+	
+}
